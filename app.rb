ed1fed65ef65d1735d2c6b185a76a18a27a8a721
mulukimwangeka <mwangekaruth@gmail.com>
2022-10-17 14:25:58 +0300

    Test are running

---
+++ app.rb
@@ -0,0 +1,27 @@
+# app.rb
+puts "Hello world!"
+
+# app.rb
+puts "Hello World!"
+puts "Hello World!"
+puts "Hello World!"
+
+
+# app.rb
+print "Pass this test, please."
+print "Pass this test, please."
+print "Pass this test, please."
+print "Pass this test, please."
+
+
+
+# app.rb
+
+puts [1, 2, 3]
+
+#p method
+p [1, 2, 3]
+
+#pp method 
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
